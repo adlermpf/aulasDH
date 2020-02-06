4844a65d0fada4221a7c6366fa1a9e6c946a3edb
Adler <adlermpf@me.com>
2020-02-06 16:40:46 -0300

    a funcao buscarPetPeloNome agora retorna o nome do pet

--- indexRefatorado.js
+++ indexRefatorado.js
@@ -70,69 +70,78 @@ const exibePets = () => {
 
 exibePets(pets);
 
-// const validarDados = dadosPet => {
-//     return (
-//         dadosPet.nome &&
-//         dadosPet.idade &&
-//         dadosPet.genero &&
-//         dadosPet.tipo &&
-//         dadosPet.raca
-//     );
-// };
+const validarDados = dadosPet => {
+    return (
+        dadosPet.nome &&
+        dadosPet.idade &&
+        dadosPet.genero &&
+        dadosPet.tipo &&
+        dadosPet.raca
+    );
+};
 
-// const adicionarPets = novoPet => {
-//     if (typeof novoPet == "object" && validarDados(novoPet)) {
-//         pets.push(novoPet);
-//         novoPet.nome = novoPet.nome.toString();
-//         novoPet.idade = parseInt(novoPet.idade);
-//         if (!novoPet.servicos) {
-//             novoPet.servicos = [];
-//         }
-//         console.log("Pet adicionado com sucesso")
-//     } else {
-//         console.log("Ops, insira um pet valido");
-//     }
-// };
+const adicionarPets = novoPet => {
+    if (typeof novoPet == "object" && validarDados(novoPet)) {
+        pets.push(novoPet);
+        novoPet.nome = novoPet.nome.toString();
+        novoPet.idade = parseInt(novoPet.idade);
+        if (!novoPet.servicos) {
+            novoPet.servicos = [];
+        }
+        console.log("Pet adicionado com sucesso")
+    } else {
+        console.log("Ops, insira um pet valido");
+    }
+};
 
-// adicionarPets({
-//     nome: "Spanish",
-//     tipo: "Cão",
-//     raca: "CockerSpanel",
-//     idade: 8,
-//     genero: "Macho",
-//     servicos: ["banho", "tosa"],
-//     vacidado: true
-// });
+adicionarPets({
+    nome: "Spanish",
+    tipo: "Cão",
+    raca: "CockerSpanel",
+    idade: 8,
+    genero: "Macho",
+    servicos: ["banho", "tosa"],
+    vacidado: true
+});
 
-// const tosarPet = pet => {
-//     pet.servicos.push("tosa");
-//     console.log("Tosa realizada com sucesso");
-// };
+const tosarPet = pet => {
+    pet.servicos.push("tosa");
+    console.log("Tosa realizada com sucesso");
+};
 
-// const darBanhoPet = pet => {
-//     pet.servicos.push("banho");
-//     console.log("Banho realizada com sucesso");
-// };
+const darBanhoPet = pet => {
+    pet.servicos.push("banho");
+    console.log("Banho realizada com sucesso");
+};
 
-// const cortarUnhasPet = pet => {
-//     pet.servicos.push("Unha");
-//     console.log("Unhas cortadas!");
-// };
+const cortarUnhasPet = pet => {
+    pet.servicos.push("Unha");
+    console.log("Unhas cortadas!");
+};
 
-// const atenderPet = (pet, ...servicos) => {
-//     console.log(`Bem vindo, ${pet.nome}`);
-//     for (let servico of servicos) {
-//         servico(pet);
-//     }
-// }
+const buscarPetPeloNome = (nome) => {
+    let consultaNome = pets.filter(oPet => oPet.nome === nome);
+    return consultaNome.pop();
+}
 
-// const pagar = () => {
-//     console.log("Pagamento realizado com sucesso");
-// }
-// pagar();
-// console.log("volte Sempre");
+const atenderPet = (nome, ...servicos) => {
+    
+    
+    
+    console.log(`Bem vindo, ${pet.nome}`);
 
-// atenderPet(pets[0], darBanhoPet, cortarUnhasPet);
+    for (let servico of servicos) {
+        servico(pet);
+    }
+}
+
+const pagar = () => {
+    console.log("Pagamento realizado com sucesso");
+}
+pagar();
+console.log("volte Sempre");
+
+atenderPet(pets[0], darBanhoPet, cortarUnhasPet);
 
 
 
@@ -158,9 +167,9 @@ exibePets(pets);
 // contarVacinados(pets);
 // campanhaVacina(pets);
 
-// const buscarPetPeloNome = (nome) => {
-//     let consultaNome = pets.filter(oPet => oPet.nome === nome).length;
-//     (consultaNome >= 1) ? console.log(`O nome foi encontrado em ${consultaNome} ocorrencias`): console.log(`nome nao encontado`);
-// }
+const buscarPetPeloNome = (nome) => {
+    let consultaNome = pets.filter(oPet => oPet.nome === nome).length;
+    (consultaNome >= 1) ? console.log(`O nome foi encontrado em ${consultaNome} ocorrencias`): console.log(`nome nao encontado`);
+}
 
-// buscarPetPeloNome("Dutch");
+buscarPetPeloNome("Dutch");
